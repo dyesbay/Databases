79484b573a2dd3b73aac07a1da522ad3ffd4f5dd
Dias Yesbay <makoyesbay@gmail.com>
2018-10-17 13:17:17 +0600

    reupload labs

---
+++ lab2.sql
@@ -0,0 +1,25 @@
+﻿CREATE DATABASE lab2
+CREATE TABLE countries (
+country_id SERIAL PRIMARY KEY,
+country_name VARCHAR(50),
+region_id INTEGER,
+population INTEGER
+);
+SELECT * FROM countries
+INSERT INTO countries (country_name, region_id, population) VALUES ('QAZAQSTAN', 1, 17)
+INSERT INTO countries (country_id, country_name) VALUES (2, 'RUSSIA')
+INSERT INTO countries (region_id) VALUES (NULL) 
+
+INSERT INTO countries (country_name, region_id, population) VALUES ('CHINA', 4, 100), ('USA', 5, 300), ('GERMANY', 6, 400);
+ALTER TABLE countries ALTER COLUMN country_name SET DEFAULT 'KAZAKHSTAN'
+SELECT * FROM countries_new
+INSERT INTO countries (country_name) VALUES (DEFAULT)
+INSERT INTO countries  DEFAULT VALUES
+CREATE TABLE countries_new (LIKE countries INCLUDING DEFAULTS)
+INSERT INTO countries_new SELECT * FROM countries
+UPDATE countries SET region_id = 1 WHERE region_id is NULL; 
+SELECT * FROM countries
+UPDATE countries SET population = population * 1.10 RETURNING  population as NewPopulation;
+DELETE FROM countries WHERE population < 100;
+DELETE FROM countries_new USING countries WHERE countries_new.country_id = countries.country_id RETURNING * 
+DELETE FROM countries RETURNING * 
---
+++ lab5.sql
@@ -0,0 +1,29 @@
+﻿CREATE DATABASE lab5;
+CREATE TABLE customers (
+customer_id INTEGER PRIMARY KEY,
+cust_name VARCHAR(50),
+city VARCHAR(50),
+grade INTEGER,
+salesman_id INTEGER
+);
+INSERT INTO customers (customer_id,cust_name,city,grade,salesman_id) VALUES (3002, 'Nick', 'New York', 100, 5001),(3005, 'Gragh', 'California', 120, 5002);
+INSERT INTO orders VALUES (1, 3002, 'sos', 5001) , (2, 3005, 'sos1', 5002);
+
+SELECT * FROM customers WHERE grade > 100;
+SELECT * FROM customers WHERE grade > 100 AND city = 'New York';
+SELECT * FROM customers WHERE city = 'New York' OR grade > 100;
+SELECT * FROM customers WHERE city = 'Rome' OR city = 'Paris;
+SELECT * FROM customers WHERE city LIKE 'B%';
+
+CREATE TABLE orders (
+order_id SERIAL PRIMARY KEY,
+customer_id INTEGER REFERENCES customers(customer_id),
+descrtiption VARCHAR(50),
+purch_amt INTEGER
+);
+SELECT * FROM orders WHERE customer_id IN (SELECT customer_id FROM customers WHERE city = 'New York');
+SELECT * FROM customers WHERE customer_id IN (SELECT customer_id FROM orders WHERE purch_amt > 10);
+SELECT sum(purch_amt) FROM orders;
+SELECT avg(purch_amt) FROM orders;
+SELECT * FROM customers WHERE cust_name IS NOT NULL;
+SELECT max(purch_amt) FROM orders;
